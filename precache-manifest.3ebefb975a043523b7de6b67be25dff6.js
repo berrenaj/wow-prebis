d811da57062ba82f9fbf5b9ddc72804a142541ec
Alex <a.j.wyett@gmail.com>
2019-04-17 20:33:15 +0100

    Updates

---
+++ precache-manifest.3ebefb975a043523b7de6b67be25dff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ebb828ea9bfc0f2390a",
+    "url": "/static/css/main.47deccb6.chunk.css"
+  },
+  {
+    "revision": "2ebb828ea9bfc0f2390a",
+    "url": "/static/js/main.10d980c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0204b354675b50d511f6",
+    "url": "/static/js/2.49f74cf8.chunk.js"
+  },
+  {
+    "revision": "3d1462768ea99d691cd0e402d9713af4",
+    "url": "/index.html"
+  }
+];
